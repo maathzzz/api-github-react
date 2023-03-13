3f27eae8706f9f469638e1858c4b716955d6c195
Matheus <matheusdeamorim20@gmail.com>
2023-03-13 14:49:10 -0300

    add users info, styles changed

--- src/components/CardUser/index.jsx
+++ src/components/CardUser/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './styles.css';
 
-export default function CardUser() {
-
+export default function CardUser({ user }) {
   return (
-    <div>index</div>
+    <div className='card-user' key={user.id}>
+      <div className='text-user'>
+        <h3>{user.name}</h3>
+        <h4>{user.login}</h4>
+        <h5>{user.bio}</h5>
+      </div>
+      <div>
+        <img className='avatar' src={user.avatar_url} onClick={user.html_url}></img>
+      </div>
+    </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
+
+import { FiArrowUpCircle, FiSearch } from "react-icons/fi";
 import './styles.css';
+
 import Card from '../../components/Card';
+import CardUser from '../../components/CardUser';
 
 function Home() {
-  const [username, setUsername] = useState('maathzzz', {avatar: '', bio: ''});
+  const [username, setUsername] = useState('maathzzz');
   const [repositories, setRepositories] = useState([]);
+  const [user, setUser] = useState({});
 
-  const handleSearch = () => {
+  const handleSearch = async() => {
 
-     axios.get(`https://api.github.com/users/${username}`)
+     await axios.get(`https://api.github.com/users/${username}`)
       .then(response => {
         const user = response.data;
+        setUser(user);
         axios.get(user.repos_url)
           .then(response => {
             const repositories = response.data;
@@ -33,17 +39,25 @@ function Home() {
   return (
     <div className='container'>
       <div className='child'>
-        <div>
-          <h1> GitHub API - Search By User</h1>
+        <div className='search-user'>
+          <h1> List Repositories of GitHub </h1>
           <input type="text" 
             placeholder="Type GitHub username..." 
             value={username} 
             onChange={event => setUsername(event.target.value)} 
           />
-          <button className='search' onClick={handleSearch}>Search User</button>
+
+          {/* <FiSearch className='btn-search' onClick={handleSearch} /> */}
+          <button className='search-btn' onClick={handleSearch}>Search by User</button>
         </div>
+
+          <CardUser user={user} />
+        
       </div>
+
       <Card repositories={repositories} />
+
+      <FiArrowUpCircle className='backOnTop' onClick={() => window.scrollTo(0, 0)} />
     </div>
   );
 }
